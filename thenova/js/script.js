b3c4e06c2a88c778ef66e6dd63fdacfeb6879b88
bloodulon <97232235+Bloodulon@users.noreply.github.com>
2025-09-21 22:14:32 +0500

    Add files via upload
    Added particles, changed header, other minor changes

--- thenova/js/script.js
+++ thenova/js/script.js
@@ -22,3 +22,41 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const animatedElement = document.querySelector('.info-cards');
+
+    if (!animatedElement) {
+        return;
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    observer.observe(animatedElement);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+
+    if (!header) {
+        return;
+    }
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+
+});
