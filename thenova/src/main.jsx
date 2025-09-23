a272b6559f9343746f2ea6055adcca94ff70a818
bloodulon <97232235+Bloodulon@users.noreply.github.com>
2025-09-23 11:49:10 +0500

    modularity

--- thenova/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Roadmap from "./App.jsx"; // Импортируем компонент
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Roadmap />
-  </React.StrictMode>
-);
